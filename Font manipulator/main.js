185f68b8960141f213ef1cf22f7b661f5228c36a
112swde <82498343+112swde@users.noreply.github.com>
2022-01-05 09:31:41 +0530

    Add files via upload

--- Font manipulator/main.js
+++ Font manipulator/main.js
@@ -1,7 +1,7 @@
 function setup()
 {
     video = createCapture(VIDEO);
-    video.size(550,500);
+    video.size(500,500);
     
     canvas = createCanvas(550,550);
     canvas.position(560,150);
@@ -12,7 +12,13 @@ function setup()
 
 function draw()
 {
-    background("#000000");
+    background("#FFFFFF");
+    textSize(difference);
+    fill("#00FF00");
+    stroke("#937256");
+    text("Pen",50,300);
+
+    document.getElementById("info").innerHTML = "width and height of the text will be " + difference + "px";
 }
 
 function modelLoaded()
@@ -25,5 +31,22 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX =" + noseX + "noseY =" + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("left wrist = " + leftWristX + "right wrist = " + rightWristX + "difference = " + difference);
+
     }
 }
+
+leftWristX = 0;
+rightWristX = 0;
+difference = 0;
+noseX = 0;
+noseY = 0;
